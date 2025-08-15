514ca8223469994f9f38f5bed3847a8d866ae115
parm-samx <parm2102000@gmail.com>
2025-08-15 16:27:00 +0530

    Create service.js

---
+++ private-chat/service.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const http = require("http");
+const { Server } = require("socket.io");
+const path = require("path");
+
+const app = express();
+const server = http.createServer(app);
+const io = new Server(server);
+
+app.use(express.static(path.join(__dirname, "public"))); // serve frontend
+
+io.on("connection", (socket) => {
+  console.log("User connected");
+
+  socket.on("chat_message", (msg) => {
+    io.emit("chat_message", msg); // send message to all
+  });
+
+  socket.on("disconnect", () => {
+    console.log("User disconnected");
+  });
+});
+
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
